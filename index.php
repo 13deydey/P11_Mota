4ddcb37aeee2a1319582777d54488e505749dc29
Audrey Picarel <aud.picarel@gmail.com>
2025-10-23 15:47:36 +0200

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<?php get_header(); ?>
+
+<main>
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            ?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <div>
+                    <?php the_content(); ?>
+                </div>
+            </article>
+            <?php
+        endwhile;
+    else :
+        ?>
+        <p>Aucun article trouvé.</p>
+        <?php
+    endif;
+    ?>
+</main>
+
+<?php get_footer(); ?>
