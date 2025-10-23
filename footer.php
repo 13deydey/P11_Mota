3d850183b49f2b9f27ef0ae7f9e0131c26c37cd4
Audrey Picarel <aud.picarel@gmail.com>
2025-10-23 15:49:23 +0200

    header & footer complétés

---
+++ footer.php
@@ -0,0 +1,14 @@
+</body>
+<footer>
+    <div class="footer__content">
+        <p>&copy; <?php echo date("Y"); ?> Nathalie Mota. Tous droits réservés.</p>
+        <nav class="footer__menu">
+            <ul>
+                <li><a href="#mentionsLegales">MENTIONS LÉGALES</a></li>
+                <li><a href="#viePrivee">VIE PRIVÉE</a></li>
+                <li><a href="#droitsReserves">TOUS DROITS RÉSERVÉS</a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Site de Nathalie Mota</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <div class="nav__logo">
+                <a href=" ">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/Logo.png" alt="Logo Mota Photo">
+                </a>
+        </div>
+        <nav class="nav__menu">
+            <ul>
+                <li><a href="#home">ACCUEIL</a></li>
+                <li><a href="#about">À PROPOS</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
