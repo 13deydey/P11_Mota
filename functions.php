2e8daec9a70e1aa97cb797c37d411c4a875eed41
Audrey Picarel <aud.picarel@gmail.com>
2025-10-24 13:45:05 +0200

    wp_enqueue fonts.css et main.css + sass

--- functions.php
+++ functions.php
@@ -1,15 +1,27 @@
 <?php
-function mon_theme_styles() {
-    // Enregistre et met en file d'attente votre feuille de style principale
-    wp_enqueue_style( 
-        'mon-style-principal', // Un nom unique (handle) pour votre style
-        get_template_directory_uri() . '/style.css', // Le chemin d'accès dynamique
-        array(), // Les dépendances (autres styles à charger avant, ici aucun)
-        wp_get_theme()->get('Version') // Version pour éviter les problèmes de cache
+function mon_theme_enqueue_styles() {
+    wp_enqueue_style(
+        'theme-id-style',  // Handle: Un nom pour l'identifier
+        get_stylesheet_uri(), // Chemin: style.css à la racine
+        array(),
+        wp_get_theme()->get('Version')
+    );
+
+    //2. ENQUEUE FONTS 
+    wp_enqueue_style(
+        'theme-fonts',
+        get_template_directory_uri() . '/css/font.css',
+        array('theme-id-style'),
+        '1.0'
+    );
+
+    //3. ENQUEUE MAIN CSS
+    wp_enqueue_style(
+        'theme-main-css',
+        get_template_directory_uri() . '/css/main.css',
+        array('theme-fonts'),
+        '1.0'
     );
 }
-add_action( 'wp_enqueue_scripts', 'mon_theme_styles' );
-//charge ma feuille CSS de déclaration init et mes polices d'écriture
-
-//je devrais charger aussi ma feuille de style w/ sass 
+add_action('wp_enqueue_scripts', 'mon_theme_enqueue_styles');
 ?>   
